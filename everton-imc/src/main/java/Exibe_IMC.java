a7903b2bdb91a8232fdaaba4d585a95e7603141c
Everton <evertonid@ymail.com>
2016-04-17 16:10:26 -0300

    Criação da classe para exibir o IMC

---
+++ everton-imc/src/main/java/Exibe_IMC.java
@@ -0,0 +1,18 @@
+
+public class Exibe_IMC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		IMC imc = new IMC();
+		 
+		 double resultado = imc.CalculaIMC(1.80, 80);
+		 
+		 System.out.println("O resultado do IMC para altura de 1.80 e peso de 80 quilos é: " + resultado);
+		
+		
+		
+		
+	}
+
+}
